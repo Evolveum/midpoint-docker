21f0fde84c049c12469f542e714dd4571339467b
Kamil Jires <kamil@jires.eu>
2021-02-09 15:46:21 +0100

    update related to MID-6802 & MID-6807

--- container_files/usr-local-bin/start-midpoint.sh
+++ container_files/usr-local-bin/start-midpoint.sh
@@ -23,7 +23,31 @@ then
 	check REPO_PASSWORD_FILE;	
 fi
 
-java -Xmx$MP_MEM_MAX -Xms$MP_MEM_INIT -Dfile.encoding=UTF8 \
+if [ $( grep "\-cp" ${MP_DIR}/bin/midpoint.sh | wc -l ) -eq 1 ]
+then
+  java -Xmx$MP_MEM_MAX -Xms$MP_MEM_INIT -Dfile.encoding=UTF8 \
+       -Dmidpoint.home=$MP_DIR/var \
+       -Dmidpoint.repository.database=$REPO_DATABASE_TYPE \
+       $(if [ -n "$REPO_USER" ]; then echo "-Dmidpoint.repository.jdbcUsername=$REPO_USER"; fi) \
+       $(if [ -n "$REPO_PASSWORD_FILE" ]; then echo "-Dmidpoint.repository.jdbcPassword_FILE=$REPO_PASSWORD_FILE"; fi) \
+       -Dmidpoint.repository.jdbcUrl="`$MP_DIR/repository-url`" \
+       -Dmidpoint.repository.hibernateHbm2ddl=none \
+       -Dmidpoint.repository.missingSchemaAction=$REPO_MISSING_SCHEMA_ACTION \
+       -Dmidpoint.repository.upgradeableSchemaAction=$REPO_UPGRADEABLE_SCHEMA_ACTION \
+       $(if [ -n "$REPO_SCHEMA_VERSION_IF_MISSING" ]; then echo "-Dmidpoint.repository.schemaVersionIfMissing=$REPO_SCHEMA_VERSION_IF_MISSING"; fi) \
+       $(if [ -n "$REPO_SCHEMA_VARIANT" ]; then echo "-Dmidpoint.repository.schemaVariant=$REPO_SCHEMA_VARIANT"; fi) \
+       -Dmidpoint.repository.initializationFailTimeout=60000 \
+       $(if [ -n "$MP_KEYSTORE_PASSWORD_FILE" ]; then echo "-Dmidpoint.keystore.keyStorePassword_FILE=$MP_KEYSTORE_PASSWORD_FILE"; fi) \
+       -Dmidpoint.logging.alt.enabled=true \
+       $MP_JAVA_OPTS \
+       -cp $MP_DIR/lib/midpoint.war \
+       -Dloader.path="WEB-INF/classes,WEB-INF/lib,WEB-INF/lib-provided,${MP_DIR}/lib/" \
+       org.springframework.boot.loader.PropertiesLauncher \
+       "$@"
+
+else
+
+  java -Xmx$MP_MEM_MAX -Xms$MP_MEM_INIT -Dfile.encoding=UTF8 \
        -Dmidpoint.home=$MP_DIR/var \
        -Dmidpoint.repository.database=$REPO_DATABASE_TYPE \
        $(if [ -n "$REPO_USER" ]; then echo "-Dmidpoint.repository.jdbcUsername=$REPO_USER"; fi) \
@@ -39,3 +63,5 @@ java -Xmx$MP_MEM_MAX -Xms$MP_MEM_INIT -Dfile.encoding=UTF8 \
        -Dmidpoint.logging.alt.enabled=true \
        $MP_JAVA_OPTS \
        -jar $MP_DIR/lib/midpoint.war
+
+fi
