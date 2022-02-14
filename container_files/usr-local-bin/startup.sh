81b3aed4bd0d43ee82145ec36b379ebce8f1bb6e
Kamil Jires <kamil@jires.eu>
2022-02-14 12:50:26 +0100

    remove obsolete files related to former docker image structure

--- container_files/usr-local-bin/startup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-/usr/local/bin/log-timezone.sh
-
-if [ $(grep -c "container" ${MP_DIR}/bin/midpoint.sh) -eq 0 ]; then \
-	${MP_DIR}/bin/midpoint.sh
-else
-	/usr/local/bin/start-midpoint.sh
-fi
